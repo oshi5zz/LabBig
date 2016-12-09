d4728344d2c941087f03a53b6b35fc1d62dd661e
oshi5zz <609254979@qq.com>
2016-12-09 14:21:43 +0800

    Update main.js

--- res/zhouzhe/main.js
+++ res/zhouzhe/main.js
@@ -13,15 +13,6 @@ define( [
 /**
  * Load a url into a page
  */
-jQuery.fn.load = function( url, params, callback ) {
-	var selector, type, response,
-		self = this,
-		off = url.indexOf( " " );
-
-	if ( off > -1 ) {
-		selector = stripAndCollapse( url.slice( off ) );
-		url = url.slice( 0, off );
-	}
 
 	// If it's a function
 	if ( jQuery.isFunction( params ) ) {
